e526778b1f87ac999d2226b48a84670862b6c7de
Cristhian <cdelgado@lean-tech.io>
2023-05-09 14:18:51 -0500

    add string connection and config it

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 // global imports
 global using net_template_web_api.Models;
+global using Microsoft.EntityFrameworkCore;
 using net_template_web_api.Services.CharacterService;
+using net_template_web_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
